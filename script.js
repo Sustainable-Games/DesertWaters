575cb28e3871298c763717524b39793d4bc948a3
Dan Stormont <daniel.stormont@gmail.com>
2025-04-25 20:11:44 -0700

    Add turn meter.

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-// JavaScript code can be added here
-console.log("Page loaded");
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
+const ctx = document.getElementById("map-canvas").getContext("2d");
+const img = new Image();
+const turnMeter = document.getElementById("turn-meter");
+const endTurnButton = document.getElementById("end-turn-button");
+
+var gameYear = 2025;
+
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0);
 });
+
+img.src = "img/Arizona_relief_map.png";
+
+function init() {
+    turnMeter.value = gameYear;
+}
+
+function endTurn() {
+    gameYear++;
+    turnMeter.value = gameYear;
+
+    if (gameYear >= 2050) {
+        gameOver();
+    }
+}
+
+function gameOver() {
+    console.log("Game over!");
+}
+
+endTurnButton.addEventListener("click", endTurn);
